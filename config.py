2a1d1b8490d86ef33e19e5a2f888abbbdf78d58c
Danila <icy.calmness@gmail.com>
2023-08-08 04:41:14 +0300

    correct QUERIES_PATH in config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import psycopg2
 # connect to db.
 CONNECT_DB = psycopg2.connect(host='localhost', database='hh_job_parser', user='postgres', password='6464')
 
-QUERIES_PATH = str(Path(__file__).resolve().parent / "database" / "queries.sql")
+QUERIES_PATH = str(Path(__file__).resolve().parent / "src" / "database" / "queries.sql")
 
 EMPLOYERS_VACANCY_ID = {
     "VK": 15478,
