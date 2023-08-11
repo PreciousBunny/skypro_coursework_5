3f097cf9a13a3a57cf225aa7adb956b670c06106
Danila <icy.calmness@gmail.com>
2023-08-11 04:28:25 +0300

    add def read_queries_sql; src.database.read_queries_sql.py

---
+++ src/database/read_queries_sql.py
@@ -0,0 +1,22 @@
+from config import QUERIES_PATH
+
+
+def read_queries_sql():
+    """
+    Функция читает и сохраняет в словаре SQL-запросы из файла.
+    """
+    queries_sql = {}
+    current_query_name = None
+    current_query = ""
+
+    with open(QUERIES_PATH, "r", encoding="utf-8") as file:
+        for line in file:
+            if line.startswith("--"):
+                if current_query_name and current_query:
+                    queries_sql[current_query_name] = current_query.strip()
+                current_query_name = line.lstrip("--").strip()
+                current_query = ""
+            else:
+                current_query += line
+
+    return queries_sql
