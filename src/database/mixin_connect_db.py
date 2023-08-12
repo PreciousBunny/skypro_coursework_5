2d12904c1f990e9deef73e55bf42a5cb1358dcb7
Danila <icy.calmness@gmail.com>
2023-08-12 05:49:42 +0300

    add class MixinConnectDB; src.database.mixin_connect_db.py

---
+++ src/database/mixin_connect_db.py
@@ -0,0 +1,29 @@
+import os
+import sys
+sys.path.append(os.getcwd())
+from config import CONNECT_DB
+
+
+class MixinConnectDB:
+    """
+    Миксин класс предоставляет методы для подключения и отключения базы данных PostgreSQL.
+    """
+
+    def connect_db(self) -> None:
+        """
+        Метод позволяет подключаться к базе данных PostgreSQL.
+        """
+        try:
+            self.connected = CONNECT_DB
+            print("database connected.")
+        except Exception as ex:
+            print("Error connecting to the database:", ex)
+
+    def disable_db(self) -> None:
+        """
+        Метод позволяет отключаться от базы данных PostgreSQL.
+        """
+        if self.connected:
+            self.connected.close()
+            print("Database is disabled.")
+
